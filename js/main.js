a114bda3adf83ebdce2e8f6ac8e8b2adb0c41abc
vikytrusowa <vikytrusowa.@gmail.com>
2018-11-21 13:31:57 +0200

    first commit

---
+++ js/main.js
@@ -0,0 +1,7 @@
+
+$('.dashboard__heading--button').on('click', function () {
+    $('.modal').removeClass('none')
+});
+$('.title__button').on('click', function () {
+    $('.modal').addClass('none')
+});
---
+++ static/js/main.js
@@ -0,0 +1,1 @@
+$(".dashboard__heading--button").on("click",function(){$(".modal").removeClass("none")}),$(".title__button").on("click",function(){$(".modal").addClass("none")});
